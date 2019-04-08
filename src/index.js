65ce9b61d925f06de38cc1a5ec665b868c45a50e
Badwaka <1456683844@qq.com>
2019-04-08 16:06:42 +0800

    bak

--- src/index.js
+++ src/index.js
@@ -9,9 +9,12 @@ const {
 
 /**
  * 解析 .vue 文件
+ *
  * @param {string} filePath 要解析的 .vue 文件路径
+ *
+ * @return {object} docObj 返回一个数据对象
  */
-let parseVue = (filePath) => {
+const parseVue = (filePath) => {
 
     // 转换成绝对路径
     filePath = path.resolve(__dirname, filePath);
@@ -20,6 +23,7 @@ let parseVue = (filePath) => {
     let extname = path.extname(filePath);
     if (extname !== '.vue') {
         console.error('不是 .vue 文件');
+        return null;
     }
 
     // vue 文件内容字符串
@@ -33,6 +37,11 @@ let parseVue = (filePath) => {
 
     // 用 eval 执行 js 部分，得到 js 对象
     let jsObj = eval(babel.transformSync(sfcObj.script.content).code);
+    // fs.writeFile('./test/jsObj.json', JSON.stringify(jsObj), (err) => {
+    //     if (err) {
+    //         console.log('err', err);
+    //     }
+    // });
 
     // 用 jsdoc-api 解析，得到 jsdoc 解析结果对象
     let jsdocObj = jsdocApi.explainSync({
@@ -55,9 +64,49 @@ let parseVue = (filePath) => {
     return docObj;
 };
 
+/**
+ * 写入 markdown
+ *
+ * @param {object} docObj 文档 json 对象
+ * @param {string} dirPath 文件夹路径
+ */
+const writeMD = (docObj, dirPath) => {
+
+    // 转换成绝对路径
+    dirPath = path.resolve(__dirname, dirPath);
+    console.log('dirPath', dirPath);
+
+    let mdPath = `${dirPath}/README.md`;
+    let mdContent = '## API\naaa';
+
+    fs.readFile(mdPath, 'utf8', (err, data) => {
+        // 文件不存在
+        if (err) {
+            fs.writeFile(mdPath, mdContent, 'utf8', () => {
+                console.log('没有 README.md，写入成功');
+            });
+        }
+        // 文件存在
+        else {
+            let index = data.indexOf('## API');
+            fs.truncate(mdPath, index, (err) => {
+                if (!err) {
+                    console.log('删除成功');
+                }
+                // 追加文件
+                fs.appendFile(mdPath, mdContent, 'utf8', () => {
+                    console.log('有 README.md，写入成功');
+                });
+            });
+        }
+    });
+
+};
+
 module.exports = {
     parseVue
 };
 
 let test = '../test/test.vue';
-parseVue(test);
+let docObj = parseVue(test);
+writeMD(docObj, '../test');
---
+++ test/README.md
@@ -0,0 +1,4 @@
+# test
+
+## API
+aaa
