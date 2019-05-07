f8b5d72b9f40c7d50907bc2f472877f7dd26eaa6
Badwaka <1456683844@qq.com>
2019-05-07 16:26:36 +0800

    bak

---
+++ src/jsdoc-vue-api-cli.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+const path = require('path');
+const jsdocVueApi = require('./jsdoc-vue-api');
+console.log('jsdoc-vue-api-cli', 'process.argv', process.argv);
+
+/**
+ * vue 文件路径
+ */
+let vueFilePath = process.argv[2];
+// 如果没有扔一个 error
+if (!vueFilePath) {
+    throw "I need .vue file!"
+}
+// 得到绝对路径
+vueFilePath = path.resolve(__dirname, vueFilePath);
+console.log('vueFilePath', vueFilePath);
+
+/**
+ * readme 文件存放路径
+ */
+let readmeDirPath = process.argv[3];
+// 如果没有，就在 .vue 文件当前目录下创建
+if (!readmeDirPath) {
+    let vueFilePathArr = vueFilePath.split('/');
+    vueFilePathArr.splice(vueFilePathArr.length - 1, 1);
+    readmeDirPath = vueFilePathArr.join('/');
+}
+readmeDirPath = path.resolve(__dirname, readmeDirPath);
+console.log('readmeDirPath', readmeDirPath);
+
+// 得到文档数据对象
+let docObj = jsdocVueApi.parseVue(vueFilePath);
+
+// 写入 markdown
+jsdocVueApi.writeMD(docObj, readmeDirPath);
+
